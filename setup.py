db66b02bb65ba3d4a9b2af92c42095965c1b7969
derfenix <derfenix@gmail.com>
2014-04-03 02:40:16 +0400

    fix setup

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='wiin',
-    version='',
+    version='0.1.0',
     packages=['wiin'],
     url='',
     requires=[
-        'SQLAlchemy(>=0.9)', 'psycopg2', 'Flask-Restless', 'flask(==0.10)',
-        'python-dateutil(>2.0)', 'mimerender(>=0.5.2)', 'Flask-SQLAlchemy'
+        'SQLAlchemy(>=0.9)', 'psycopg2', 'Flask_Restless', 'flask(==0.10)',
+        'python_dateutil(>2.0)', 'mimerender(>=0.5.2)', 'Flask_SQLAlchemy'
     ],
     license='',
     author='derfenix',
